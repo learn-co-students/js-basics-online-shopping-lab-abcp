ad314d3667ab6f8ee8864d785df7f139fafbd1e1
Jay Bednar <ironviralmedia@gmail.com>
2018-01-15 02:45:48 +0000

    Done.

--- index.js
+++ index.js
@@ -21,17 +21,26 @@ return cart;
 }
 
 function viewCart() {
-  if (cart[0] === undefined){
-    return 'Your shopping cart is empty.';
-  } else {
-    for (var i = 0, i < cart.length; i++) {
-      
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.');
+  } else if(cart.length === 1) {
+    var i = 0;
+    var keys = Object.keys(cart[i]) 
+    var cartItem = Object.keys(cart[i]);
+    var itemPrice = Object.values(cart[i]);
+    var message = `In your cart, you have ${cartItem} at $${itemPrice}`;
+    console.log(`${message}.`);
+  } else if (cart.length === 2) {
+    for (i = 0; i < cart.length; i++) {
+      var newMessage = `${message} and ${cartItem} at $${itemPrice}`;
     }
+    console.log(`${newMessage}.`)
   }
 }
 
+
 function total() {
-  // write your code here
+  
 }
 
 function removeFromCart(item) {
