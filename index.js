f9c82eec064e424f62f1cafe1a6ca16495fff1be
CJ Haviland <cj.haviland@yahoo.com>
2018-01-16 21:57:57 +0000

    Done.

--- index.js
+++ index.js
@@ -11,18 +11,73 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var randPrice = Math.floor(Math.random() * 100 + 1);
+ 
+ cart.push(Object.assign({}, { [item]: randPrice }));
+ 
+ console.log(`${item} has been added to your cart.`);
+ 
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  var cartString = `In your cart, you have`;
+  let cartLen = cart.length;
+  
+  for (var i = 0; i < cartLen; i++){
+    
+    // Oxford comma it up
+    if(i > 0 && cartLen > 2){
+      cartString += `,`;
+    }
+    
+    // Add an 'and' for more than 1 item or last item
+    if (i > 0 && i === cartLen - 1){
+      cartString += ` and`;
+    }
+    
+    cartString += ` ${Object.keys(cart[i])} at $${Object.values(cart[i])}`;
+  }
+  
+  cartString += `.`;
+  
+  if (cartLen > 0){
+    console.log(cartString);
+  }
+  else{
+    console.log(`Your shopping cart is empty.`);
+  }
+  
+  return cart;
 }
 
 function total() {
   // write your code here
+  var cartTotal = 0;
+  let cartLen = cart.length;
+  
+  for (var i = 0; i < cartLen; i++){
+    cartTotal += Object.values(cart[i])[0];
+  }
+  
+  return cartTotal;
 }
 
 function removeFromCart(item) {
   // write your code here
+  let cartLen = cart.length;
+  
+  for (var i = 0; i < cartLen; i++){
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(i, 1);
+    }
+    else{
+      console.log(`That item is not in your cart`);
+    }
+  }
+  
+  return cart;
 }
 
 function placeOrder(cardNumber) {
