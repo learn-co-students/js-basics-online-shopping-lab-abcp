6369ac4b3faa89bf57f8cd0cafde998dfeae2fe4
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-16 08:53:17 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,80 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var ran = Math.floor(Math.random() * 100);
+  var newObj = {[item]: ran};
+  cart.push(newObj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var inCart = "In your cart, you have ";
+  if (cart.length > 0) {
+    
+    var soloKeyA = Object.keys(cart[0]);
+    var soloKeyB = soloKeyA[0];
+    
+    if (cart.length === 1) {
+      inCart += `${soloKeyB} at $${cart[0][soloKeyB]}.`;
+      console.log(inCart);
+    }
+    
+    for (let i = 0; i < cart.length; ++i) {
+      
+      var keyA = Object.keys(cart[i]);
+      var keyB = keyA[0];
+      
+      if (i === cart.length-1) {
+        inCart += `and ${keyB} at $${cart[i][keyB]}.`;
+      } else {
+        if (cart.length === 2) {
+          inCart += `${keyB} at $${cart[i][keyB]} `;
+        } else {
+          inCart += `${keyB} at $${cart[i][keyB]}, `;
+        }
+      }
+      
+    }
+    console.log(inCart);
+  } else {
+    console.log('Your shopping cart is empty.');
+  }
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (let i = 0; i < cart.length; ++i) {
+    var keyA = Object.keys(cart[i]);
+    var keyB = keyA[0];
+    
+    sum += cart[i][keyB];
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; ++i) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log('That item is not in your cart.');
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber == null) {
+    console.log('Sorry, we don\'t have a credit card on file for you.');
+    return;
+  }
+  
+  var sum = total();
+  if (parseInt(cardNumber, 10) && cardNumber.toString().length === 8) {
+    console.log(`Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`);
+    cart.splice(0);
+  } else {
+    return 'You did not put in a valid, 8-digit credit card number';
+  }
 }
