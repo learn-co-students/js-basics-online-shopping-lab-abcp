50af5c3220283d4de2e527dbe7c2caa5b6d6eb4f
Jay Bednar <ironviralmedia@gmail.com>
2018-01-17 01:13:16 +0000

    Done.

--- index.js
+++ index.js
@@ -23,16 +23,17 @@ return cart;
 function viewCart() {
 	var itemsInCart = [];
 	var i = 0;
+	var message = `In your cart, you have`;
 	for (i = 0; i < cart.length; i++) {
 			itemsInCart.push([Object.keys(cart[i]), cart[i][Object.keys(cart[i])]]);
 		}
 	if (cart.length === 0){
 		console.log('Your shopping cart is empty.');
 	} else if (cart.length === 1) {
-		console.log(`In your cart, you have ${itemsInCart[0][0]} at $${itemsInCart[0][1]}.`);
+		console.log(`${message} ${itemsInCart[0][0]} at $${itemsInCart[0][1]}.`);
 	} else if (cart.length === 2) {
-		console.log(`In your cart, you have ${itemsInCart[0][0]} at $${itemsInCart[0][1]} and ${itemsInCart[1][0]} at $${itemsInCart[1][1]}.`);
-	} /* else if (cart.length >= 3) {
+		console.log(`${message} ${itemsInCart[0][0]} at $${itemsInCart[0][1]} and ${itemsInCart[1][0]} at $${itemsInCart[1][1]}.`);
+	}/* else {
 		for (i = 0; i < cart.length; i++) {
 			var innerCart = itemsInCart[i].join(' at $');	
 		} var outerCart =  {
@@ -54,13 +55,14 @@ function total() {
 	} 
 	return sum;
 }	
-/*
+
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
-    var cartNew = {Object.keys(cart[i]): cart[i][Object.keys(cart[i])]};
-  }
-  console.log(cartNew);
-} */
+    var itemPresent = cart[i].hasOwnProperty(item);
+  } if (itemPresent === false){
+    console.log(`That item is not in your cart.`);
+  } 
+} 
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
